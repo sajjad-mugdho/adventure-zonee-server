3087884fd062c7c023eab51c03f94f365da45fc2
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-11-16 15:50:07 +0600

    mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');   
 const app = express();
 require('dotenv').config();
@@ -10,6 +11,21 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2psefg9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function run() {
+    try {
+        
+    } 
+    finally {
+        
+    }
+}
+
+run()
 
 app.get('/', (req, res) => {
     res.send('Adventure Zone Server Running')
