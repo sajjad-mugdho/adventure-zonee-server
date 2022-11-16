1a8826d0d189a0e5c5b30311d08b1cb9b1932939
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-11-17 03:38:28 +0600

    service page done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');   
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
@@ -18,10 +18,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 function run() {
     try {
-        
-    } 
+        const serviceCollection = client.db('adventure-zone-assignment').collection('tour-services')
+
+        // Service Api
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+
+    }
     finally {
-        
+
     }
 }
 
