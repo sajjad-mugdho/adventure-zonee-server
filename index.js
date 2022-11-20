7ccf1195ca4c6ebeb9bab20fffd703f0e2e1b9c8
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-11-20 17:07:52 +0600

    update added

--- index.js
+++ index.js
@@ -52,13 +52,13 @@ function run() {
         // Review Api
         app.get('/reviews', async(req, res) =>{
             let query = {};
+            
             if (req.query.email) {
-                const query = {
+                query = {
                     email: req.query.email
                 }
-            };
+            }
             const cursor = reviewCollection.find(query);
-
             const reviews = await cursor.toArray();
             res.send(reviews)
         })
@@ -68,16 +68,24 @@ function run() {
             res.send(result);
         })
 
+        app.patch('/reviews/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result)
+        })
+
         app.delete('/reviews/:id' , async(req, res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await reviewCollection.deleteOne(query);
             res.send(result)
         })
-
-        
-
-
     }
     finally {
 
